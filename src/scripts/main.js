08bf8f27a1ab599e4a3f43cc0b78e359f6ca2343
Sasha Popovych <sashapopovych98@gmail.com>
2024-12-19 12:20:41 +0100

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+function convertToNumber(stringSalary) {
+  return parseFloat(stringSalary.replace(/[^\d.-]/g, ''));
+}
+
+function sortList(list) {
+  const itemsList = [...list.children];
+
+  itemsList.sort((a, b) => {
+    const salaryA = convertToNumber(a.dataset.salary);
+    const salaryB = convertToNumber(b.dataset.salary);
+
+    return salaryA - salaryB;
+  });
+
+  list.innerHTML = '';
+
+  itemsList.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = [...list.children];
+
+  return items.map((i) => ({
+    name: i.dataset.name,
+    position: i.dataset.position,
+    salary: convertToNumber(i.dataset.salary),
+  }));
+}
+sortList();
+getEmployees();
